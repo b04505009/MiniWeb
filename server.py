df32f2e0ec79ffa74fc959fa9d20d5059fec1195
劉錫臻 <liuxizhen@liuxizhendeMacBook-Pro.local>
2018-05-30 17:35:26 +0800

    test is upload

--- server.py
+++ server.py
@@ -1,22 +1,36 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2018  <@DESKTOP-TA60DPH>
-#
-# Distributed under terms of the MIT license.
-
-"""
-Here is the access point into web service
-"""
-from flask import Flast,request
+from flask import Flask, render_template, request
+from os.path import join, dirname
+from werkzeug.utils import secure_filename
+import tempfile
 
 server = Flask(__name__)
 
+
 @server.route('/')
-def index():
-    return "Hello World"
+@server.route('/home')
+def home():
+    return render_template('home.html')
 
 
-if __name__ == '__main__':
-    server.run(debug=True)
+@server.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@server.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        print(request.files.get('upload'))
+        is_upload = False
+        if request.files.get('upload') != None:
+            upload = request.files.get('upload')
+            dir_name = tempfile.mkdtemp()
+            upload.save(dir_name + '/' + secure_filename(upload.filename))
+            is_upload = True
+            file_dir_name = str(dir_name + '/' +
+                                secure_filename(upload.filename))
+            print(file_dir_name)
+    return render_template('result.html', file_dir_name=file_dir_name)
+
+
+server.run(port=5000, debug=True)
